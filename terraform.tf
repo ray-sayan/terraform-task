90bf0999f5f58e357c1456c5eb3d4917f0a5dd52
Sayan Ray <35499694+ray-sayan@users.noreply.github.com>
2026-04-22 15:25:59 +0530

    Configure S3 backend for Terraform state
    Added S3 backend configuration for Terraform state management.

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.40.0"
     }
   }
+  backend "s3" {
+    bucket         = "sayan-terraform-state-12345"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile   = true   # ✅ NEW locking 
+    encrypt        = true
+  }
 }
